3ef7d0347b40382eece95b38d6bd94ff2ed3b2b9
DeHch0 <denisrumenov98@gmail.com>
2021-03-24 23:50:29 +0200

    Footer cmp created

---
+++ src/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react';
+import './style.css';
+
+const Footer = () => {
+
+    useEffect( () => {
+
+    } , )
+
+    return (
+        <div className="footer">
+            <span>Copyright @ 2021 GSM App. All rights reserved.</span>
+        </div>
+    )
+}
+
+export default Footer
